80782d11c230d056a78f10bdbe85345a1cc6f8ba
“Landon <landonwjohnson@gmail.com>
2023-03-11 10:02:41 -0700

    Initial Commit

---
+++ LeetCode/LeetCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeetCodeApp.swift
+//  LeetCode
+//
+//  Created by Landon Johnson on 3/11/23.
+//
+
+import SwiftUI
+
+@main
+struct LeetCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
